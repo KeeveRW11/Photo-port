4ee2701e3512042a14e74d18e1e7c13e9628417b
KeeveRW11 <keevewhyte+github@gmail.com>
2022-11-09 11:53:54 -0500

    set test to about and nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,36 +1,26 @@
 function Nav() {
     const categories = [
-        {
-            name: "commercial",
-            description:
-                "Photos of the grocery stores, food trucks, and other commercial projects",
-        },
+        { name: "commercial",description:"Photos of the grocery stores, food trucks, and other commercial projects"},
         { name: "portraits", description: "Portraits of people in my life" },
         { name: "food", description: "Delicious delicacies" },
-        {
-          name: "landscape",
-          description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-        },
+        { name: "landscape", description: "Fields, farmhouses, waterfalls, and the beauty of nature"},
     ];
 
     function categorySelected(name) {
         console.log(`${name} clicked`)
     };
 
-
-
-
     return (
         <header>
             <h2>
-             <a href="/">
+             <a data-testid="link" href="/">
                 <span role="img" aria-label="camera"> 📸</span> Oh Snap!
              </a>
             </h2>
-            <nav>
+            <nav>               
               <ul className="flex-row">
                 <li className="mx-2">
-                  <a href="#about">
+                  <a data-testid="about" href="#about">
                     About me
                   </a>
                 </li>
