4295be0ee6c08d67629fe9d524e00fa32e0e3bdc
KeeveRW11 <keevewhyte+github@gmail.com>
2022-11-11 14:56:00 -0500

    conditional render contact and run test

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 import { useState } from 'react';
 
+
+
 function App() {
   const [categories] = useState([
     {
@@ -19,17 +21,26 @@ function App() {
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
+  const [contactSelected,setContactSelected] =useState(false);
+
   return (
     <div>
-      <Nav
+      <Nav 
         categories={categories}
         setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory}>
-      </Nav>
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
       <main>
-        <ContactForm></ContactForm>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,17 +1,11 @@
-import React, {useState } from 'react';
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
 
-    
+    const [errorMessage, setErrorMessage] = useState('');
 
     const [formState, setFormState ] = useState({ name: '', email: '', message: '' });
-
-    function handleChange(e) {
-        setFormState({ ...formState, [e.target.name]: [e.target.value] })
-    }
-
-    // console.log(formState)
-
     const { name, email, message } = formState;
     
     function handleSubmit(e) {
@@ -19,23 +13,52 @@ function ContactForm() {
         console.log(formState);
     }
 
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            if(!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if(!e.target.value) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+
+        if(!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: [e.target.value] })
+        }
+
+        console.log('errorMessage', errorMessage);
+    }
+
     return (
         <section>
-            <h1>Contact me</h1>
+            <h1 data-testid="h1tag"> Contact Me</h1>
             <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" defaultValue={formState.name} onChange={handleChange} name="name" />
+                    <input type="text" defaultValue={name} onBlur={handleChange} name="name" />
                 </div>
                 <div>
                 <label htmlFor="email">Email address:</label>
-                <input type="email" defaultValue={formState.name} onChange={handleChange} name="email" />
+                <input type="email" defaultValue={email} onBlur={handleChange} name="email" />
                 </div>
                 <div>
                 <label htmlFor="message">Message:</label>
-                <textarea name="message" defaultValue={formState.name} onChange={handleChange} rows="5" />
+                <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5" />
                 </div>
-                <button type="submit">Submit</button>
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}  </p>
+                    </div>
+                )}
+                <button data-testid="button" type="submit">Submit</button>
             </form>
         </section>
     );
