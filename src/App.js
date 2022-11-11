fe681d7f01e1507f27876ed89c9410d8ae67510b
KeeveRW11 <keevewhyte+github@gmail.com>
2022-11-11 17:09:25 -0500

    modal now opens

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import logo from './logo.svg';
 // import './App.css';
+import { useState } from 'react';
+
 import Nav from './components/Nav'
 import About from './components/About'
 import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
-import { useState } from 'react';
 
 
 
